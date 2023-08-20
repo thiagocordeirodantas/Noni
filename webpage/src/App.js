f125559d1d28a9626a6178832c9baf5351e03046
Thiago <thiagocoordeirodantas@gmail.com>
2023-08-20 20:46:32 -0300

    Componente Header finalizado

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import Navegacao from "./components/Navegacao";
 
 function App() {
   return (
    <div>
       <Navegacao/>
+      <Header/>
     </div>
   );
 }
--- webpage/src/components/Header/index.js
+++ webpage/src/components/Header/index.js
@@ -4,6 +4,24 @@ const Header = () => {
     return (
         <div className="header-container">
 
+            <section className="header-container-titulo">
+                <h1 className="titulo-header">The Most Quirky Writings Delivered Right To Your Inbox</h1>
+            </section>
+
+            <div className="container-input-header">
+
+            <section className="header-container-conteudo-input">
+                <input type="email" name="email" id="email" placeholder="Enter your email address"  className="input-header"/>
+                <a href="" className="link-input-header">Subscribe</a>
+            </section>
+        </div>
+            <section className="icones-patrocinio">
+                <img className="icones-patrocinio-item" src="Group 57.svg" alt="imagem"/>
+                <img className="icones-patrocinio-item" src="Group 58.svg" alt="imagem"/>
+                <img className="icones-patrocinio-item" src="Group 59.svg" alt="imagem"/>
+
+                <img className="icones-patrocinio-item" src="Vector.svg" alt="imagem"/>
+            </section>
         </div>
     )
 }
